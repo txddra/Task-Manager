fee62d2181d0bb08c1554b3223b25254dac06e38
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-21 12:48:24 -0600

    creating context

--- src/context/TaskListContext.js
+++ src/context/TaskListContext.js
@@ -1,3 +1,12 @@
 import React,{ createContext, useState } from "react";
 
 export const TaskListContext = createContext();
+
+const TaskListContextProvider =()=>{
+
+    const [tasks, setTasks] =useState("")
+
+    return <div>Task List Context</div>
+};
+
+export default TaskListContextProvider
