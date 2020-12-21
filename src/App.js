a31be79768045515945597547e935e33bae476cd
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-21 13:23:31 -0600

    importing TaskList to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import TaskList from './components/TaskList'
+import "./App.css"
 // import TaskList from './'
 
 function App() {
   return (
     <div className="App">
-
-        app
+<TaskList />
+        
     </div>
   );
 }
