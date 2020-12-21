32c31686ef305d0906c0833dc925e335cc023127
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-21 14:27:45 -0600

    added a function to delete the task

--- src/context/TaskListContext.js
+++ src/context/TaskListContext.js
@@ -1,5 +1,5 @@
 import React,{ createContext, useState } from "react";
-// import uuid from "uuidv4";
+// import uuid from "uuid";
 
 export const TaskListContext = createContext();
 
@@ -10,11 +10,23 @@ const TaskListContextProvider =(props)=>{
         {title: "Walk doggie", id:2},
         {title: "Make lunch", id:3}
 
-    ])
+    ]);
+
+    const addTask =(title)=>{
+        //making a brand new array
+        setTasks([...tasks,{title, 
+            // id:uuid()
+        }])
+
+    };
+
+    const removeTask  =()=>{
+        setTasks(task.filter(task=>{task.id !== id}))
+    }
 
     return(
 // value will have all the data from this state  
-        <TaskListContext.Provider value={{tasks}}>
+        <TaskListContext.Provider value={{tasks, addTask, removeTask}}>
             {/* all the components that will be wrapped in the provider */}
           {props.children}
             </TaskListContext.Provider>
