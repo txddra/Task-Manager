b10f1698ca363c65eb87f0173c6be15e35329655
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-21 13:21:40 -0600

    added buttons to task file

---
+++ src/components/Task.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Task = () => {
+    return (
+        <li className = "list-item">
+            <span> Task Title</span>
+            //buttons will add and delete
+            <button className ="btn-delete task-btn"><i className="fas fa-trash-alt"></i> </button>
+            <button className ="btn-delete task-btn"><i className="fas fa-pen"></i> </button>
+        </li>
+            
+        
+    )
+}
+
+export default Task
