e15c3ea092de6626c4b6a3663dbf50e8107781cd
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-21 14:18:20 -0600

    adding funcitons to the butttons

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import { TaskListContext } from "../context/TaskListContext";
 
 const TaskForm = () => {
+
+    const {addTask} = useContext(TaskListContext);
+
+    const [title, setTitle] = useState("")
+
+    const handleChange= (e)=>{
+
+        setTitle(e.target.value)
+    };
+
+    const handleSubmit =(e)=>{
+        e.preventDefault()
+    }
     return (
-        <form className ="form">
+        <form 
+        onSubmit ={handleSubmit}
+            className ="form">
             <input
+            onChange= {handleChange}
             type="text"
             className="task-input"
             placeholder= "Add new task"
